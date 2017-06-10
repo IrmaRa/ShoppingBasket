281024e73d32507d5d9ac4c901a987342bc5b6f0
Irma Rasikeviciute <rasikeviciute@yahoo.com>
2017-06-10 15:52:16 +0100

    Add addItem method

--- specs/BasketTest.java
+++ specs/BasketTest.java
@@ -7,15 +7,26 @@ import basket.*;
 public class BasketTest {
 
   Basket basket;
+  Wine item1;
+  Cheese item2;
 
   @Before
   public void before() {
     basket = new Basket();
+    item1 = new Wine("Merlot", 17);
+    item2 = new Cheese("Taleggio", 8);
   }
 
   @Test
   public void canCountItemsInBasket() {
     assertEquals(0, basket.basketCount());
+  }
+
+  @Test
+  public void canAddItemToBasket() {
+    basket.addItem(item1);
+    basket.addItem(item2);
+    assertEquals(2, basket.basketCount());
   }
 
 }
